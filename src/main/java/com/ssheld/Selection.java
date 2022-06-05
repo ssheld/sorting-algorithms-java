d8f15f8531ded8eb529434fe0513a6e4723b57a0
ssheld <20730278+ssheld@users.noreply.github.com>
2022-06-05 16:35:13 -0400

    implement selection sort

--- src/main/java/com/ssheld/Selection.java
+++ src/main/java/com/ssheld/Selection.java
@@ -1,4 +1,16 @@
 package com.ssheld;
 
 public class Selection {
+
+    public static void sort(Comparable[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (SortingUtils.less(arr[j], arr[i])) {
+                    min = j;
+                }
+            }
+            SortingUtils.swap(arr, i, min);
+        }
+    }
 }

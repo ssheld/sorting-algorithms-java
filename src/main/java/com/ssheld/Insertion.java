95e40107a71b6d118096f06cb537d86727d022bf
ssheld <20730278+ssheld@users.noreply.github.com>
2022-06-10 15:35:35 -0400

    implement insertion sort

--- src/main/java/com/ssheld/Insertion.java
+++ src/main/java/com/ssheld/Insertion.java
@@ -1,2 +1,12 @@
-package com.ssheld;public class Insertion {
+package com.ssheld;
+
+public class Insertion {
+
+    public static void sort(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0 && SortingUtils.less(arr[j], arr[j-1]); j--) {
+                SortingUtils.swap(arr, j, j-1);
+            }
+        }
+    }
 }
--- src/test/java/SortingTest.java
+++ src/test/java/SortingTest.java
@@ -1,9 +1,12 @@
+import com.ssheld.Insertion;
 import com.ssheld.Selection;
 import com.ssheld.SortingUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortingTest {
 
@@ -14,9 +17,18 @@ public class SortingTest {
         arr = new Integer[]{3, -2, 5, 0, -4, 30, 12, 9, -22};
     }
 
+    // Selection sort test
     @Test
     void selectionSort() {
         Selection.sort(arr);
-        SortingUtils.isSorted(arr);
+        System.out.println(arr);
+        assertEquals(true, SortingUtils.isSorted(arr));
+    }
+
+    // Insertion sort test
+    @Test
+    void insertionSort() {
+        Insertion.sort(arr);
+        assertEquals(true, SortingUtils.isSorted(arr));
     }
 }
